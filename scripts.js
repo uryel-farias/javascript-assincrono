62e3d1c788d0fd7616de6d42b5ff293a2d94fbe2
Uryel Farias <uryel.farias@gmail.com>
2025-03-24 23:37:38 -0300

    removendo as tegs

--- scripts.js
+++ scripts.js
@@ -55,3 +55,10 @@ inputTags.addEventListener("keypress", (evento) => {
         }
     }
 })
+
+listaTags.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("remove-tag")) {
+        const tagQueQueremosRemover = evento.target.parentElement;
+        listaTags.removeChild(tagQueQueremosRemover);
+    }
+})
