c4c4d8b34aca2d3ee1c29e185a695c64fc0fbbcf
Uryel Farias <uryel.farias@gmail.com>
2025-03-28 22:05:30 -0300

    Tratando o envio do formulário.

--- scripts.js
+++ scripts.js
@@ -87,3 +87,38 @@ inputTags.addEventListener("keypress", async (evento) => {
         }
     }
 })
+
+const botaoPublucar = document.querySelector(".botao-publicar");
+
+async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const deuCerto = Math.random() > 0.5;
+
+            if (deuCerto) {
+                resolve("Projeto publicado com sucesso.")
+            } else {
+                reject("Erro ao publicar o projeto.")
+            }
+        }, 2000)
+    })
+}
+
+botaoPublucar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.querySelector("#nome").value;
+    const descricaoDoProjeto = document.querySelector("#descricao").value;
+
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo!")
+    } catch (error) {
+        console.log("Deu errado: ", error)
+        alert("Deu tudo errado!");
+    }
+
+})
